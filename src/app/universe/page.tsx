8efcbd927eb2348e7571de7077392ba52367ebeb
loliallen <maxim.kasakin@yahoo.com>
2023-10-10 17:32:03 +0300

    added space

--- src/app/universe/page.tsx
+++ src/app/universe/page.tsx
@@ -104,7 +104,7 @@ const UniversePage = () => {
         <DownloadSection />
       </div>
 
-      <section className="flex flex-col max-w-[1080px] mx-auto gap-[70px] md:max-w[650px] sm:max-w[350px] md:mx-[20px]">
+      <section className="flex flex-col max-w-[1080px] mx-auto gap-[70px] md:max-w[650px] sm:max-w[350px] md:mx-[20px] mt-[50px] md:mt-[30px]">
         <div className="bg-card p-[70px_50px] flex flex-col gap-[50px] items-center md:p-[50px_30px] md:rounded-[40px] md:gap-[30px]">
           <div className="text-accentGreen text-[30px] leading-[37.8px] font-[300] md:text-[24px] md:leading-[30.24px]">
             Introducing UNIVERSE
